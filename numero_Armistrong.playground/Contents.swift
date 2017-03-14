5fa35937fe28d825337a35bf558e49d3cbd0a9f5
Victor Stillo <victor.stillo@gmail.com>
2017-03-14 17:39:13 -0300

    Exercices first class

--- numero_Armistrong.playground/Contents.swift
+++ numero_Armistrong.playground/Contents.swift
@@ -3,15 +3,17 @@
 import UIKit
 
 func isArmstrongNumber(n : Int) -> Bool {
-    var result = false
-    var digits : String = String(n)
+    //var result = false
+    var digits : [String] = String(n).characters.map {String($0)}
+    var sum = 0.0
     
-    for digit in digits.characters {
-        var d = Int(digit)
+    for digit in digits {
+        //print(digit)
+        sum += pow(Double(digit)!, Double(digits.count))
         
     }
     
-    return result
+    return Int(sum) == n
 }
 
 var n = 153
